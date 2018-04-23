d59088cae4ef7adfd394e1cbb9c0c1010d8666dd
Unknown <quannd@blockchainlabs.asia>
2018-04-23 20:07:34 +0700

    commit

---
+++ Login_SignUp_ScreenDesgin_Demo/Login_SignUp_ScreenDesgin_Demo/app/src/main/java/com/login_signup_screendesign_demo/Course_Frament.java
@@ -0,0 +1,60 @@
+package com.login_signup_screendesign_demo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+/**
+ * Created by nguyendangquan on 14/04/2018.
+ */
+
+public class Course_Frament extends Fragment implements View.OnClickListener {
+    private static View view;
+    private static Button all_course ;
+    private static ListView listview ;
+    private static FragmentManager fragmentManager;
+    private final String arr[] = {"Active Learning","PPL"} ;
+    //private ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.activity_list_item,arr) ;
+
+    public Course_Frament(){
+
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.course, container,
+                false);
+
+
+        initViews();
+        setListeners();
+        return view ;
+    }
+    private void initViews() {
+        fragmentManager = getActivity().getSupportFragmentManager();
+        all_course =(Button) view.findViewById(R.id.bt2) ;
+        listview = (ListView) view.findViewById(R.id.listview) ;
+    }
+    private void setListeners() {
+        all_course.setOnClickListener(this);
+    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt2:
+                //listview.setAdapter(adapter);
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.right_enter, R.anim.left_out)
+                        .replace(R.id.frameContainer,
+                                new MainWork_Fragment(),
+                                Utils.MainWork_Fragment).commit();
+                break ;
+        }
+    }
+
+}
---
+++ Login_SignUp_ScreenDesgin_Demo/Login_SignUp_ScreenDesgin_Demo/app/src/main/java/com/login_signup_screendesign_demo/Utils.java
@@ -0,0 +1,13 @@
+package com.login_signup_screendesign_demo;
+
+public class Utils {
+	
+	//Email Validation pattern
+	public static final String regEx = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
+
+	//Fragments Tags
+	public static final String Login_Fragment = "Login_Fragment";
+	public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
+	public static final String MainWork_Fragment = "MainWork_Fragment";
+	public static final String Course_Fragment = "Course_Frament";
+}
